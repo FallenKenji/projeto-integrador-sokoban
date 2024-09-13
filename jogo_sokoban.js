4bc9ba9a85577ba80091a3b09a3c9effc60b8115
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-13 17:03:16 -0300

    Atualização: Linha e Quadrado

---
+++ jogo_sokoban.js
@@ -0,0 +1,11 @@
+const board2 = document.getElementById('board2');
+const game = document.getElementById('game');
+const row = document.createElement('div');
+
+row.classList.add('row');
+board2.appendChild(row);
+console.log(row)
+console.log(game);
+
+
+console.log("funcionou");
