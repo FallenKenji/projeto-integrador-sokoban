2e689982d0afb890f2e785a3178b99a7ce2b80fc
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-16 14:27:21 -0300

    Atualização 8x8

--- jogo_sokoban.js
+++ jogo_sokoban.js
@@ -1,15 +1,12 @@
 const tabuleiro = document.getElementById('board2');
 
-const linha = document.createElement('div');
-linha.classList.add('row');
-
-for(let i=0; i<4; i++) {}
-const celula = document.createElement('div');
-celula.classList.add('cell');
-linha.appendChild(celula);
-for(let i = 0; i<4; i++) {
-tabuleiro.appendChild(linha);
+for(let i = 0; i<8; i++) {
+    const linha = document.createElement('div');
+    linha.classList.add('row');
+    tabuleiro.appendChild(linha);
+    for(let i=0; i<8; i++) {
+        const celula = document.createElement('div');
+        celula.classList.add('cell');
+        linha.appendChild(celula); 
+    }    
 }
-
-
-tabuleiro.appendChild(linha);
