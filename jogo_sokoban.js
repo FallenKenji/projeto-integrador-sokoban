c1e53281c0617e1cb7d4705366ba6fd2f8b27dc2
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-16 16:02:48 -0300

    Arualização JavaScript 8x8

--- jogo_sokoban.js
+++ jogo_sokoban.js
@@ -1,12 +1,16 @@
 const tabuleiro = document.getElementById('board2');
 
-for(let i = 0; i<8; i++) {
-    const linha = document.createElement('div');
-    linha.classList.add('row');
-    tabuleiro.appendChild(linha);
-    for(let i=0; i<8; i++) {
-        const celula = document.createElement('div');
-        celula.classList.add('cell');
-        linha.appendChild(celula); 
-    }    
+function tab(linhas, celulas) {
+    for(let k=0; k<linhas; k++){
+        const linha = document.createElement('div');
+        linha.classList.add('row');
+        tabuleiro.append(linha);
+        for(let i=0; i<celulas; i++) {
+            const celula = document.createElement('div');
+            celula.classList.add('cell');
+            linha.append(celula);
+        }
+    }
 }
+
+tab(8, 8);
