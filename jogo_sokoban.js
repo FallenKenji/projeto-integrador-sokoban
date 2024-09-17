a117e746583e299617df88eb4abf3e5ed70bc028
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-17 17:01:10 -0300

    Adiciona Funções para criar Tabuleiros

--- jogo_sokoban.js
+++ jogo_sokoban.js
@@ -1,44 +1,104 @@
-const tabuleiro = document.getElementById('board2');
+function buildGameBoard(linhas, celulas, regra) {
+    const game = document.getElementById("game");
+    const board = document.createElement('div');
+    board.classList.add('board');
 
-function tab(linhas, celulas) {
-    for(let y = 0; y < linhas; y++){
+    for (let y = 0; y < linhas; y++) {
         const linha = document.createElement('div');
         linha.classList.add('row');
-        tabuleiro.append(linha);
-        for(let x = 0; x < celulas; x++) {
+        board.append(linha);
+
+        for (let x = 0; x < celulas; x++) {
             const celula = document.createElement('div');
             celula.classList.add('cell');
             linha.append(celula);
 
-            // if (y == 0 || y == linhas - 1 || x == 0 || x == celulas - 1) {
-            //     celula.classList.add('empty');
-            // } 
-
-            // if (y > 0 && y < linhas - 1 && x > 0 && x < celulas - 1) {
-            //     celula.classList.add('empty');
-            // }
-
-            // if(y > 0 && y < 7 && x > 0 && x < 7 ){
-            //     celula.classList.add('empty');
-            //   }
-            
-            // if (y == x || y + x === linhas - 1) {
-            //     celula.classList.add('empty');
-            // }
-
-            // if (y == x) {
-            //     celula.classList.add('empty');
-            // }
-
-            // if (y + x == linhas -1) {
-            //     celula.classList.add('empty'); 
-            // }
-
+            regra(celula, linhas, celulas, y, x);
+        
         }
+    }
+
+    game.append(board);
+}
+function regra(celula, linhas, celulas, y, x) {
+    if (y == 0 || y == 7 || x == 0 || x == 7) {
+        celula.classList.add('empty');
+    }   
+}
+
+function regra1(celula, linhas, celulas, y, x) {
+    if (y > 0 && y < 7 && x > 0 && x < 7) {
+        celula.classList.add('empty');
     }
 }
 
-tab(8, 8);
+function regra2(celula, linhas, celulas, y, x) {
+    if (y + x == 7) {
+        celula.classList.add('empty');
+    }
+}
+
+function regra3(celula, linhas, celulas, y, x) {
+    if (y == x) {
+        celula.classList.add('empty');
+    }
+}
+
+function regra4(celula, linhas, celulas, y, x) {
+    if (y == x || y + x == 7) {
+        celula.classList.add('empty');
+    }
+}
+
+function regra5(celula, linhas, celulas, y, x) {
+    if ((y + x) % 2 == 0) {
+        celula.classList.add('empty')
+    }
+}
 
 
+buildGameBoard(8, 8, regra);
+buildGameBoard(8, 8, regra1)
+buildGameBoard(8, 8, regra2)
+buildGameBoard(8, 8, regra3)
+buildGameBoard(8, 8, regra4)
+buildGameBoard(8, 8, regra5)
 
+
+// const tabuleiro = document.getElementById('board2');
+
+// function tab(linhas, celulas) {
+//     for(let y = 0; y < linhas; y++){
+//         const linha = document.createElement('div');
+//         linha.classList.add('row');
+//         tabuleiro.append(linha);
+//         for(let x = 0; x < celulas; x++) {
+//             const celula = document.createElement('div');
+//             celula.classList.add('cell');
+//             linha.append(celula);
+
+//             if (y == 0 || y == 7 || x == 0 || x == 7) {
+//                 celula.classList.add('empty');
+//             } 
+
+//             if(y > 0 && y < 7 && x > 0 && x < 7 ){
+//                 celula.classList.add('empty');
+//               }
+
+//             if (y + x == 7) {
+//                 celula.classList.add('empty'); 
+//             }
+
+//             if (y == x) {
+//                 celula.classList.add('empty');
+//             }
+            
+//             if (y == x || y + x == 7) {
+//                 celula.classList.add('paint');
+//             }
+
+//         }
+//     }
+// }
+
+// tab(8, 8);
