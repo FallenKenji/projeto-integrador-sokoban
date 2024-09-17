fcfa8491cf4a4e565f980fc0eef6eca8cb69a511
Milton <cleitinyeagerrumbling4@gmail.com>
2024-09-17 10:37:11 -0300

    Retirando o centro e deixando a borda, retirando em X, ambas diagonais. Atualização no arquivo de test e melhorias no codigo principal

--- jogo_sokoban.js
+++ jogo_sokoban.js
@@ -1,21 +1,41 @@
 const tabuleiro = document.getElementById('board2');
 
 function tab(linhas, celulas) {
-    for(let k=0; k<linhas; k++){
+    for(let y = 0; y < linhas; y++){
         const linha = document.createElement('div');
         linha.classList.add('row');
         tabuleiro.append(linha);
-        for(let i=0; i<celulas; i++) {
+        for(let x = 0; x < celulas; x++) {
             const celula = document.createElement('div');
             celula.classList.add('cell');
             linha.append(celula);
 
-            if (k==0 || k == 7 || i == 0 || i == 7  ) {
-                celula.classList.add('empty');    
+            // if (y == 0 || y == linhas - 1 || x == 0 || x == celulas - 1) {
+            //     celula.classList.add('empty');
+            // } 
+
+            // if (y > 0 && y < linhas - 1 && x > 0 && x < celulas - 1) {
+            //     celula.classList.add('empty');
+            // }
+            
+            // if (y == x || y + x === linhas - 1) {
+            //     celula.classList.add('empty');
+            // }
+
+            // if (y == x) {
+            //     celula.classList.add('empty');
+            // }
+
+            if (y + x == linhas -1) {
+                celula.classList.add('empty');
+                
             }
+
         }
     }
 }
 
 tab(8, 8);
+
+
 
