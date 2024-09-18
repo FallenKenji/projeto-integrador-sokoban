21291600970ae14a02fa27b1bc02104d3c856a0b
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-18 17:30:32 -0300

    Movimentação de Coordenadas

--- sokoban.js
+++ sokoban.js
@@ -1,6 +1,33 @@
+let x = 3
+let y = 4
+console.log(x, y);
+
 const player = document.querySelector('.player');
 console.log(player);
 
 player.addEventListener("click", function(){
-    alert("Clicou no Jogador Zero");
+    window.alert("Clicou no Jogador Zero");
+})
+
+window.addEventListener("keydown", function(event){
+    // window.alert("Pressionou tecla");
+    console.log(event)
+
+    if (event.code == 'ArrowUp' && x > 0) {
+        x--;
+    }
+
+    if (event.code == 'ArrowDown' && x < 4) {
+        x++;
+    }
+
+    if (event.code == 'ArrowRight' && y < 4 ) {
+        y++;
+    }
+
+    if (event.code == 'ArrowLeft' && y > 0) {
+        y--;
+    }
+    console.log(event.code, x, y);
+
 })
