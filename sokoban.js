8a5ddeb54f102231a1a6af451c1f2a50082f4912
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-19 17:34:41 -0300

    Movendo o Player

--- sokoban.js
+++ sokoban.js
@@ -13,12 +13,17 @@ playerElement.addEventListener("click", function() {
 window.addEventListener("keydown", function(event) {
     nextPosition(event.code);
 })
+const celulas = document.querySelectorAll('.cell');
+console.log(celulas);
 
 function nextPosition(keycode) {
-        if (keycode == 'ArrowUp' && player.x > 0) player.x--;
-        if (keycode == 'ArrowDown' && player.x < 4) player.x++;
-        if (keycode == 'ArrowRight' && player.y < 4) player.y++;
-        if (keycode == 'ArrowLeft' && player.y > 0) player.y--;
+    if (keycode == 'ArrowUp' && player.x > 0) player.x--;
+    if (keycode == 'ArrowDown' && player.x < 4) player.x++;
+    if (keycode == 'ArrowRight' && player.y < 4) player.y++;
+    if (keycode == 'ArrowLeft' && player.y > 0) player.y--;
         
-        console.log(keycode, player);
+    console.log(keycode, player);
+    const K = player.x * 4 + player.y;
+    celulas[K].append(playerElement);
 }
+
