42a1da748b29a5d31ca245eb6c1d1c248c6532f1
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-19 16:51:46 -0300

    Função nextPosition

--- sokoban.js
+++ sokoban.js
@@ -1,33 +1,34 @@
-let x = 3
-let y = 4
+let x = 0
+let y = 0
 console.log(x, y);
 
 const player = document.querySelector('.player');
 console.log(player);
 
-player.addEventListener("click", function(){
+player.addEventListener("click", function() {
     window.alert("Clicou no Jogador Zero");
 })
 
-window.addEventListener("keydown", function(event){
+window.addEventListener("keydown", function(event) {
     // window.alert("Pressionou tecla");
-    console.log(event)
-
-    if (event.code == 'ArrowUp' && x > 0) {
-        x--;
-    }
-
-    if (event.code == 'ArrowDown' && x < 4) {
-        x++;
-    }
-
-    if (event.code == 'ArrowRight' && y < 4 ) {
-        y++;
-    }
-
-    if (event.code == 'ArrowLeft' && y > 0) {
-        y--;
-    }
-    console.log(event.code, x, y);
-
+    nextPosition(event.code);
 })
+
+function nextPosition(keycode) {
+        if (keycode == 'ArrowUp' && x > 0) {
+            x--;
+        }
+    
+        if (keycode == 'ArrowDown' && x < 4) {
+            x++;
+        }
+    
+        if (keycode == 'ArrowRight' && y < 4 ) {
+            y++;
+        }
+    
+        if (keycode == 'ArrowLeft' && y > 0) {
+            y--;
+        }
+        console.log(keycode, x, y);
+}
