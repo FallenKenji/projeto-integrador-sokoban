897f8267864aee05cf587f799f2d1f0c38477e7c
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-20 17:04:23 -0300

    Transformando as funções em this.

--- sokoban.js
+++ sokoban.js
@@ -1,33 +1,33 @@
-window.addEventListener("keydown", function(event) {
-    const next = nextPosition(event.code);
+const player = new Player(0, 0)
+const playerElement = document.querySelector('.player');
+const celulas = document.querySelectorAll('.cell');
 
-    if(verifyPosition(next)) movePlayer(next);
+window.addEventListener("keydown", function(event) {
+    const next = player.nextPosition(event.code);
+    if(verifyPosition(next)) player.movePlayer(next);
 })
 
 function Player(posX, posY) {
     this.x = posX;
     this.y = posY;
+
+    this.nextPosition = function(keycode) {
+        let {x, y} = this;
+        if (keycode == 'ArrowUp') x--;
+        if (keycode == 'ArrowDown') x++;
+        if (keycode == 'ArrowRight') y++;
+        if (keycode == 'ArrowLeft') y--;   
+        return {x, y};
+    }
+    
+    this.movePlayer = function(position) {
+        this.x = position.x;
+        this.y = position.y;
+        const K = this.x * 4 + this.y;
+        celulas[K].append(playerElement);    
+    }
 }
 
-const player = new Player(0, 0)
-const playerElement = document.querySelector('.player');
-const celulas = document.querySelectorAll('.cell');
-
-function nextPosition(keycode) {
-    let {x, y} = player;
-    if (keycode == 'ArrowUp') x--;
-    if (keycode == 'ArrowDown') x++;
-    if (keycode == 'ArrowRight') y++;
-    if (keycode == 'ArrowLeft') y--;   
-    return {x, y};
-}
-function movePlayer(position) {
-    let {x, y} = position;
-    player.x = x;
-    player.y = y;
-    const K = player.x * 4 + player.y;
-    celulas[K].append(playerElement);    
-}
 function verifyPosition(position) {
     let {x, y} = position;
     return x >= 0 && x < 4 && y >= 0 && y < 4;
