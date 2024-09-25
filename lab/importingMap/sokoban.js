5d1c1c7493aa7970f48e8a6cd8f354bbc04e738f
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-25 17:11:14 -0300

    Adição de Wall, Box, Goal e atualização de codigo

--- lab/importingMap/sokoban.js
+++ lab/importingMap/sokoban.js
@@ -1,34 +1,40 @@
 const boardMap = [
-    [ "#", "#", "#", "#", "#", "#", "#", "#" ],
-    [ "#", ".", ".", ".", ".", ".", ".", "#" ],
-    [ "#", ".", ".", ".", "#", ".", ".", "#" ],
-    [ "#", ".", "#", "G", ".", ".", ".", "#" ],
-    [ "#", ".", ".", "G", "B", "#", ".", "#" ],
-    [ "#", ".", ".", "#", ".", "B", ".", "#" ],
-    [ "#", ".", ".", "P", ".", ".", ".", "#" ],
-    [ "#", "#", "#", "#", "#", "#", "#", "#" ]
+    ["#", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", ".", ".", ".", ".", ".", ".", "#"],
+    ["#", ".", ".", ".", "#", ".", ".", "#"],
+    ["#", ".", "#", "G", ".", ".", ".", "#"],
+    ["#", ".", ".", "G", "B", "#", ".", "#"],
+    ["#", ".", ".", "#", ".", "B", ".", "#"],
+    ["#", ".", ".", "P", ".", ".", ".", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "#"]
 ]
-    function parede () {
-    if(boardMap[0][7] == '#' )
-        cell.classList.add('wall');
-    }
 
 const NUM_ROWS = boardMap.length;
 const NUM_COLS = boardMap[0].length;
-buildGameBoard(NUM_ROWS, NUM_COLS,);
+buildGameBoard(NUM_ROWS, NUM_COLS);
+
 function createGameElement(elementName, className, parentNode) {
     const element = document.createElement(elementName)
     element.classList.add(className);
     parentNode.append(element);
+
     return element;
 }
 function buildGameBoard(linhas, celulas, regras) {
     const game = document.getElementById('game');
+
     const board = createGameElement('div', 'board', game);
+    
     for (let k = 0; k < linhas; k++) {
-        const linha = createGameElement('div','row', board);
+        const linha = createGameElement('div', 'row', board);
+
         for (let i = 0; i < celulas; i++) {
-            const celula = createGameElement('div','cell', linha);
+            const celula = createGameElement('div', 'cell', linha);
+            const char = boardMap[k][i];
+            
+            if (char === '#')celula.classList.add('wall');
+            if (char === 'B')celula.classList.add('box');
+            if (char === 'G')celula.classList.add('goal');
         }
     }
 }
