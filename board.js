77a6bf7792c4e427b5ded95beadccdf863c37bf3
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-09-30 17:29:24 -0300

    Atualização do Código, colocando as caixas para movimentar

--- board.js
+++ board.js
@@ -21,27 +21,29 @@ export function createGameElement(elementName, className, parentNode) {
 }
 
 export function buildGameBoard() {
-    const positionOfPieces = {};
+    const pieces = {
+        boxes: []
+    };
 
     const game = document.getElementById('game');
     const board = createGameElement('div', 'board', game);
 
     for (let i = 0; i < NUM_ROWS; i++) {
-        const linha = createGameElement('div', ['row'], board);
+        const linha = createGameElement('div', 'row', board);
 
         for (let j = 0; j < NUM_COLS; j++) {
-            const celula = createGameElement('div', ['cell'], linha);
+            const celula = createGameElement('div', 'cell', linha);
 
             const char = boardMap[i][j];
             const position = {x: j, y: i};
 
-            if (char === '#') celula.classList.add(['wall']);
-            if (char === 'B') celula.classList.add(['box']);
-            if (char === 'G') celula.classList.add(['goal']);
-            if (char === 'P') positionOfPieces.player = position;
-
+            if (char === '#') celula.classList.add('wall');
+            // if (char === 'B') celula.classList.add(['box']);
+            if (char === 'G') celula.classList.add('goal');
+            if (char === 'P') pieces.player = position;
+            if (char === 'B') pieces.boxes.push(position);
         }
     }
 
-    return positionOfPieces;
+    return pieces;
 }
--- sokoban.js
+++ sokoban.js
@@ -5,21 +5,28 @@ const pieces = buildGameBoard();
 const board = document.querySelector('.board')
 
 const playerPiece = createBoardPiece(pieces.player, 'player');
+const boxesPiece = [];
+    for (let i = 0; i < pieces.boxes.length; i++){
+        boxesPiece.push(createBoardPiece(pieces.boxes[i], 'box'))
+    };
 
+window.addEventListener("keydown", function (event) {
+        handleKeydownEvent(event.code);
+});
+    
 function createBoardPiece(piecePosition, className) {
     const piece = new Piece(piecePosition.x, piecePosition.y);
     piece.insertElementInto(className, board);
-    
     return piece;
 }
 
-window.addEventListener("keydown", function (event) {
+function handleKeydownEvent(keycode) {
     const next = playerPiece.nextPosition(event.code);
 
     if (verifyPosition(next)) {
         playerPiece.moveTo(next);
     }
-});
+}
 
 function verifyPosition(position) {
     let {x: i , y: k} = position;
